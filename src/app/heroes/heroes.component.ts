84dd40955c76327d8d7a5c0d4ab2a69f3028d315
Juan <72524993+jdlopez-dev@users.noreply.github.com>
2021-12-29 10:09:58 +0100

    refactor: :recycle: access the hero detail via link

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { MessageService } from '@app/message.service';
-import { Hero } from '../hero';
-import { HeroService } from '../hero.service';
+import { Hero } from '@app/hero';
+import { HeroService } from '@app/hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,19 +8,12 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  selectedHero?: Hero;
   heroes: Hero[] = [];
-  constructor(
-    private heroService: HeroService,
-    private messageService: MessageService
-  ) {}
+
+  constructor(private heroService: HeroService) {}
+
   ngOnInit(): void {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
